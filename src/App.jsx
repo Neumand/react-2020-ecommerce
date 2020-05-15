3afe2822c1117fa1bdb91bc9d1c782ea515b5032
David Neuman <david.m.neuman@gmail.com>
2020-05-15 07:00:31 -0400

    Implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.scss';
+
 import HomePage from './pages/home/home';
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => <div>HATS</div>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
